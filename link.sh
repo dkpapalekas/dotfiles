23f8a2146ac42ea589b8be61445090fc09153d27
dkpap <d.papalekas@doppler.gr>
2021-07-01 09:46:44 +0300

    add vscode config

--- link.sh
+++ link.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+IFS='
+'
+
+alias lns='ln -s -v --'
+
 for f in home/*
 do
-	ln -s -v -- "$(realpath $f)" "$HOME/.$(basename $f)"
+	lns "$(realpath $f)" "$HOME/.$(basename $f)"
 done
+
+lns $(realpath vscode/config.yaml) $HOME/.config/code-server/config.yaml
+lns $(realpath vscode/keybindings.json) $HOME/.local/share/code-server/User/keybindings.json
+lns $(realpath vscode/settings.json) $HOME/.local/share/code-server/User/settings.json
